e3366f7f9905073cfee86e18e71c948d53912beb
Máirín Duffy <duffy@redhat.com>
2019-06-25 15:53:34 -0400

    this doesn't compile

--- src/components/pipeline/NodeDetails.tsx
+++ src/components/pipeline/NodeDetails.tsx
@@ -1,14 +1,97 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { IPluginItem } from "../../api/models/pluginInstance.model";
-import { Button, Grid, GridItem } from "@patternfly/react-core";
+import { Button, Grid, GridItem, Modal } from "@patternfly/react-core";
+import { ModelessOverlay, Modal as PF3Modal} from "patternfly-react";
 import { ShareAltIcon, InfrastructureIcon } from "@patternfly/react-icons";
 import TreeNodeModel, { INode } from "../../api/models/tree-node.model";
 import { getPluginInstanceTitle } from "../../api/models/pluginInstance.model";
 import PipelineTree from "./PipelineTree";
+import { Props } from "tippy.js";
 interface INodeProps {
   selected: IPluginItem;
   descendants: IPluginItem[];
 }
+
+type State = {};
+
+export class MockModelessManager extends React.Component<Props, State> {
+  constructor() {
+    super(this.props, this.state);
+    this.state = { showOverlay: false};
+  }
+  close = () => {
+    this.setState({ showOverlay: false});
+  };
+  toggleOpen = () => {
+    this.setState({ showOverlay: !this.state.showOverlay });
+  };
+  render() {
+    const { children, size } = this.props;
+    const defaultBody = (
+      <form className = "form-horizontal">
+        <div className= "form-group">
+          <label className = "col-sm-3 control-label" htmlFor="textInput">
+            Field One
+          </label>
+          <div className = "col-sm-9">
+            <input type="text" id="textInput" className="form-control" />
+          </div>
+        </div>
+        <div className= "form-group">
+          <label className = "col-sm-3 control-label" htmlFor="textInput2">
+            Field One
+          </label>
+          <div className = "col-sm-9">
+            <input type="text" id="textInput" className="form-control" />
+          </div>
+        </div>
+        <div className= "form-group">
+          <label className = "col-sm-3 control-label" htmlFor="textInput3">
+            Field One
+          </label>
+          <div className = "col-sm-9">
+            <input type="text" id="textInput" className="form-control" />
+          </div>
+        </div>
+      </form>
+    );
+
+    return (
+      <div>
+        <Button bsStyle="primary" bsSize="large" onClick={this.toggleOpen}>
+          {this.state.showOverlay ? 'Close Modeless Overlay' : 'Open Modeless Overlay'}
+        </Button>
+
+        <ModelessOverlay show={this.state.showOverlay} bsSize={size !== 'default' ? size : null}>
+          <Modal.Header>
+            <Modal.CloseButton onClick={this.close} />
+            <Modal.Title>Modal Overlay Title</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>{children || defaultBody}</Modal.Body>
+          <Modal.Footer>
+            <Button bsStyle="default" className="btn-cancel" onClick={this.close}>
+              Cancel
+            </Button>
+            <Button bsStyle="primary" onClick={this.close}>
+              Save
+            </Button>
+          </Modal.Footer>
+        </ModelessOverlay>
+      </div>
+    );
+  }
+}
+
+MockModelessManager.propTypes = {
+  children: PropTypes.node,
+  size: PropTypes.string
+};
+
+MockModelessManager.defaultProps = {
+  children: null,
+  size: 'default'
+};
 
 class NodeDetails extends React.Component<INodeProps> {
   // Description: Share pipeline with others ***** Working
@@ -18,7 +101,7 @@ class NodeDetails extends React.Component<INodeProps> {
 
   // Description: Add new node to the feed ***** Working
   handleAddNewNode() {
-    // Stub - To be done
+    
   }
 
   // Description: root node or leaf nodes in the graph will not have the 'share this pipeline' button
